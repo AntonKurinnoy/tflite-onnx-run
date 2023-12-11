ed00ecae610bf4aee48257f9d9f56d6367c0bcff
AntonKurinnoy <anton.kurinnoy@gmail.com>
2023-12-11 22:33:52 +0200

    add OnnxProcessor class and implement functions

---
+++ src/main/kotlin/core/processor/onnx/OnnxProcessor.kt
@@ -0,0 +1,47 @@
+package core.processor.onnx
+
+import ai.onnxruntime.OnnxTensor
+import ai.onnxruntime.OrtEnvironment
+import ai.onnxruntime.OrtSession
+import core.processor.Processor
+import core.tensor.FloatTensor
+import core.tensor.Tensor
+import java.nio.FloatBuffer
+
+/**
+ * @author Anton Kurinnoy
+ */
+class OnnxProcessor(private val model: ByteArray) : Processor {
+
+    private lateinit var env: OrtEnvironment
+    private lateinit var session: OrtSession
+
+    override fun initialize() {
+        env = OrtEnvironment.getEnvironment()
+        session = env.createSession(model)
+    }
+
+    override fun run(input: Map<Int, Tensor>, output: MutableMap<Int, Tensor>) {
+        if (!::session.isInitialized) {
+            throw IllegalStateException("Session is not initialized. Call initialize() first.")
+        }
+
+        val inputMap = mutableMapOf<String, OnnxTensor>()
+
+        session.inputNames.forEach { name ->
+            val inputTensor = OnnxTensor.createTensor(
+                env,
+                FloatBuffer.wrap((input[0] as FloatTensor).data),
+                (input[0] as FloatTensor).shape.map { it.toLong() }.toLongArray()
+            )
+            inputMap[name] = inputTensor
+        }
+
+        val results = session.run(inputMap)
+
+        results.forEachIndexed { index, mutableEntry ->
+            output[index] = mutableEntry as Tensor
+        }
+
+    }
+}
