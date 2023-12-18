14bbbedc526b070dd3928a7e01c344e5714094e5
AntonKurinnoy <anton.kurinnoy@gmail.com>
2023-12-18 23:34:01 +0200

    add TranslatorImpl class

--- src/main/kotlin/core/translator/Translator.kt
+++ src/main/kotlin/core/translator/Translator.kt
@@ -1,7 +1,18 @@
 package core.translator
 
+import core.tensor.Tensor
+
 /**
  * @author Anton Kurinnoy
  */
 interface Translator<I, O> : PreProcessor<I>, PostProcessor<O>
 
+class TranslatorImpl<I, O> : Translator<I, O> {
+    override fun postProcessOutput(output: Map<Int, Tensor>): O {
+        TODO("Not yet implemented")
+    }
+
+    override fun preProcessInput(input: I): Map<Int, Tensor> {
+        TODO("Not yet implemented")
+    }
+}
