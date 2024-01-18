bf788500b4d6b42180969b0ddf688465c8812ff7
AntonKurinnoy <anton.kurinnoy@gmail.com>
2024-01-18 16:39:36 +0200

    add resize function realisation on buffered image

--- src/main/kotlin/net/iriscan/transform/Resize.kt
+++ src/main/kotlin/net/iriscan/transform/Resize.kt
@@ -1,6 +1,7 @@
 package net.iriscan.transform
 
 import net.iriscan.tensor.Tensor
+import java.awt.image.BufferedImage
 
 /**
  * @author Anton Kurinnoy
@@ -9,6 +10,17 @@ interface Resize : Transform
 
 class ResizeImage(private val width: Int, private val height: Int) : Resize {
     override fun transform(tensor: Tensor): Tensor {
-        TODO("Not yet implemented")
+        TODO()
+    }
+
+    companion object {
+        fun transform(image: BufferedImage, newWidth: Int, newHeight: Int): BufferedImage {
+            val resizedImage = BufferedImage(newWidth, newHeight, image.type)
+            val g = resizedImage.createGraphics()
+            g.drawImage(image, 0, 0, newWidth, newHeight, null)
+            g.dispose()
+
+            return resizedImage
+        }
     }
 }
