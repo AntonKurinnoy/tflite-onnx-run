25d5ac5652608099a0df1056916c1b2b320c1602
AntonKurinnoy <anton.kurinnoy@gmail.com>
2024-01-18 16:41:04 +0200

    Implement normalization operation

--- src/main/kotlin/net/iriscan/transform/Normalize.kt
+++ src/main/kotlin/net/iriscan/transform/Normalize.kt
@@ -1,6 +1,7 @@
 package net.iriscan.transform
 
 import net.iriscan.tensor.Tensor
+import java.nio.FloatBuffer
 
 /**
  * @author Anton Kurinnoy
@@ -9,6 +10,18 @@ interface Normalize : Transform
 
 class Normalization(private val mean: FloatArray, private val std: FloatArray) : Normalize {
     override fun transform(tensor: Tensor): Tensor {
-        TODO("Not yet implemented")
+        val size = (tensor.data as FloatBuffer).remaining()
+
+        for (i in 0 until size step 3) {
+            val red: Float = (tensor.data.get(i) - mean[0]) / std[0]
+            val green: Float = (tensor.data.get(i + 1) - mean[1]) / std[1]
+            val blue: Float = (tensor.data.get(i + 2) - mean[2]) / std[2]
+
+            tensor.data.put(i, red)
+            tensor.data.put(i + 1, green)
+            tensor.data.put(i + 2, blue)
+        }
+
+        return tensor
     }
 }
